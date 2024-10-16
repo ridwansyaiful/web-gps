5a1cab506a7fe4e4d9fbd3cd68e3f9514bf795b6
ridwansyaiful <93344629+ridwansyaiful@users.noreply.github.com>
2024-10-16 20:20:59 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing.');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Service Worker activating.');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Fetch event for ', event.request.url);
+});
