c822ede048e0af9d259c4228e62a77ecef286229
ridwansyaiful <93344629+ridwansyaiful@users.noreply.github.com>
2024-10-16 21:33:44 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,26 @@
+const cacheName = 'pwa-cache-v1';
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
+  // Tambahkan file lainnya seperti CSS, JS, dll
+];
+
+// Install Service Worker dan cache file
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('location-tracker-cache').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/monitor.html',
-                '/manifest.json',
-                '/firebase-config.js',
-                '/firebase-app.js',
-                '/firebase-database.js',
-                // Tambahkan file lain yang diperlukan
-            ]);
-        })
-    );
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
+// Mengambil resource dari cache saat offline
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
