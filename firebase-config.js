7e237f6ed1f547eaa40ac64ba4bd2d0484570579
ridwansyaiful <93344629+ridwansyaiful@users.noreply.github.com>
2024-10-16 20:21:30 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Konfigurasi Firebase Anda
+const firebaseConfig = {
+    apiKey: "AIzaSyDgPYGXqbBXt1NayhNB5M-8mjXfGKrOzWM",
+    authDomain: "coba-accf3.firebaseapp.com",
+    databaseURL: "https://coba-accf3-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    projectId: "coba-accf3",
+    storageBucket: "coba-accf3.appspot.com",
+    messagingSenderId: "171764982868",
+    appId: "1:171764982868:web:19c716f6617667514679c1"
+};
